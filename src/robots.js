30bc8087ace3207be3b5a84cb81b23d6f4cc38c5
vedant-manelkar <vedant.manelkar@somaiya.edu>
2020-07-30 03:28:14 +0530

    lol

---
+++ src/robots.js
@@ -0,0 +1,60 @@
+export const robots=[
+{
+	id: 1,
+	name: 'VEDANT MANELKAR',
+	username: 'vedantmanelkar',
+	email: 'vedantmanelkar@gmail.com'
+},
+{
+	id: 2,
+	name: 'R2-D2',
+	username: 'robot1',
+	email: 'xyz@gmail.com'
+},
+
+{
+	id: 3,
+	name: 'WALL-E',
+	username: 'robot2',
+	email: 'shadad@gmail.com'
+},
+
+{
+	id: 4,
+	name: 'HAL-9000',
+	username: 'robot3',
+	email: 'kn@gmail.com'
+},
+
+{
+	id: 5,
+	name: 'OPTIMUS PRIME',
+	username: 'robot4',
+	email: 'xyz2@gmail.com'
+},
+
+{
+	id: 6,
+	name: 'TERMINATOR',
+	username: 'robot5',
+	email: 'xyz3@gmail.com'
+}
+,
+{
+	id: 7,
+	name: 'C-3PO',
+	username: 'robot6',
+	email: 'xyz4@gmail.com'
+}
+,
+{
+	id: 8,
+	name: 'CHITTI',
+	username: 'robot7',
+	email: 'xyz5@gmail.com'
+}
+
+
+
+
+]
